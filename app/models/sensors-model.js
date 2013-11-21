8cb0c8e3cf6a0bd515781f8e3ec2f64824174194
jguerin <julian.guerin@gmail.com>
2013-11-21 13:34:07 +0100

    Change room to room_id in sensor model

--- app/models/sensors-model.js
+++ app/models/sensors-model.js
@@ -4,13 +4,13 @@ module.exports = SensorModel = Backbone.Model.extend({
     defaults : {
         name: 'MySensor',
         sensor_id: "",
-        room: "",
+        room_id: "",
     },
     schema: {
         name: 'Text',
         sensor_id: { editorAttrs: { disabled: true } },
         model: { type: 'Select', options: ['temperature', 'door'],  editorAttrs: { disabled: true }},
-        room: {type: 'Select', options: function(){
+        room_id: {type: 'Select', options: function(){
             var options = [{val: 0, label:"Unassigned"}];
             $.getJSON(
                 'http://localhost:4000/room/',
