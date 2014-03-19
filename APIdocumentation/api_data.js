8d245c97730005153372d4040a1e16d89f058c92
nmalzieu <noe.malzieu@gmail.com>
2014-03-19 21:52:15 +0100

    Updated doc to latest version

--- APIdocumentation/api_data.js
+++ APIdocumentation/api_data.js
@@ -1,135 +1,20 @@
 define({ api: [
   {
     "type": "get",
-    "url": "/rooms",
-    "title": "Read the list of rooms*",
-    "name": "GetRooms",
-    "group": "Rooms",
-    "description": "This request gives the list of rooms of one user.",
-    "success": {
-      "fields": {
-        "Success 200": [
-          {
-            "group": "Success 200",
-            "type": "String[]",
-            "field": "id",
-            "optional": false,
-            "description": "List of <code>id</code> of the rooms that belong to one user."
-          }
-        ]
-      }
-    },
-    "error": {
-      "fields": {
-        "Error 4xx": [
-          {
-            "group": "Error 4xx",
-            "field": "NoApikey",
-            "optional": false,
-            "description": "No apikey was sent with the request."
-          },
-          {
-            "group": "Error 4xx",
-            "field": "NoAccessRight",
-            "optional": false,
-            "description": "Wrong apikey."
-          }
-        ]
-      }
-    },
-    "version": "0.0.0",
-    "filename": "express/routes/room.js"
-  },
-  {
-    "type": "get",
-    "url": "/rooms/:id",
-    "title": "Read data of a room*",
-    "name": "GetRoomsId",
-    "group": "Rooms",
-    "description": "This request gives you information about a given room, for the user who is logged in.",
-    "parameter": {
-      "fields": {
-        "Parameter": [
-          {
-            "group": "Parameter",
-            "type": "String",
-            "field": "id",
-            "optional": false,
-            "description": "<code>id</code> of the room."
-          }
-        ]
-      }
-    },
-    "success": {
-      "fields": {
-        "Success 200": [
-          {
-            "group": "Success 200",
-            "type": "String",
-            "field": "id",
-            "optional": false,
-            "description": "<code>id</code> of the room."
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "field": "room",
-            "optional": false,
-            "description": "Name of the room"
-          },
-          {
-            "group": "Success 200",
-            "type": "String[]",
-            "field": "sensors",
-            "optional": false,
-            "description": "List of <code>id</code> of the sensors in the room."
-          }
-        ]
-      }
-    },
-    "error": {
-      "fields": {
-        "Error 4xx": [
-          {
-            "group": "Error 4xx",
-            "field": "IdNotFound",
-            "optional": false,
-            "description": "The <code>id</code> of the room was not found."
-          },
-          {
-            "group": "Error 4xx",
-            "field": "NoApikey",
-            "optional": false,
-            "description": "No apikey was sent with the request."
-          },
-          {
-            "group": "Error 4xx",
-            "field": "NoAccessRight",
-            "optional": false,
-            "description": "Wrong apikey."
-          }
-        ]
-      }
-    },
-    "version": "0.0.0",
-    "filename": "express/routes/room.js"
-  },
-  {
-    "type": "get",
-    "url": "/sensors",
+    "url": "/sensors/",
     "title": "Read the list of sensors*",
     "name": "GetSensors",
     "group": "Sensors",
-    "description": "This request gives the list of sensors of one user.",
+    "description": "This request gives the list of sensors of the user.",
     "success": {
       "fields": {
         "Success 200": [
           {
             "group": "Success 200",
-            "type": "String[]",
-            "field": "id",
+            "type": "Sensor[]",
+            "field": "(No-Name",
             "optional": false,
-            "description": "List of <code>id</code> of the sensors that belong to one user."
+            "description": ")          List of <code>sensors</code>, see <code>/sensors/:id</code> for a detailed structure."
           }
         ]
       }
@@ -157,7 +42,7 @@ define({ api: [
   },
   {
     "type": "get",
-    "url": "/sensors/:id",
+    "url": "/sensors/:identifier",
     "title": "Read data of a sensor*",
     "name": "GetSensorsId",
     "group": "Sensors",
@@ -165,7 +50,7 @@ define({ api: [
     "examples": [
       {
         "title": "Example usage:",
-        "content": "   curl -i http://homeagain.io/sensors/23?api_key=8264823&datastart=1391631304&datastop=1391631800\n"
+        "content": "  curl -i http://homeagain.io/sensors/23?api_key=8264823&datastart=1391631304&datastop=1391631800\n"
       }
     ],
     "parameter": {
@@ -174,9 +59,9 @@ define({ api: [
           {
             "group": "Parameter",
             "type": "String",
-            "field": "id",
+            "field": "identifier",
             "optional": false,
-            "description": "<code>id</code> of the sensor."
+            "description": "<code>identifier</code> of the sensor."
           },
           {
             "group": "Parameter",
@@ -201,44 +86,51 @@ define({ api: [
           {
             "group": "Success 200",
             "type": "String",
-            "field": "id",
+            "field": "identifier",
             "optional": false,
             "description": "<code>id</code> of the sensor."
           },
           {
             "group": "Success 200",
             "type": "String",
-            "field": "sensor",
+            "field": "name",
             "optional": false,
             "description": "Name of the sensor."
           },
           {
             "group": "Success 200",
-            "type": "String",
-            "field": "room_id",
+            "type": "Type",
+            "field": "model",
             "optional": false,
-            "description": "Id of the room where the sensor is."
+            "description": "Model used for the sensor (with a <code>name</code> argument)"
           },
           {
             "group": "Success 200",
-            "type": "String",
-            "field": "unit",
+            "type": "Id",
+            "field": "hub",
             "optional": false,
-            "description": "Unit of the data given by the sensor."
+            "description": "ObjectId of the Hub to which this sensor is linked"
           },
           {
             "group": "Success 200",
-            "type": "String",
-            "field": "data",
+            "type": "Date",
+            "field": "created_on",
             "optional": false,
-            "description": "By default, value and timestamp of the last data taken from the sensor. If a period is specified, list of all timestamp and data in this period."
+            "description": "Date the sensor was created on"
+          },
+          {
+            "group": "Success 200",
+            "type": "Id",
+            "field": "room",
+            "optional": false,
+            "description": "ObjectId of the room the sensor is assigned to"
           }
         ]
       },
       "examples": [
         {
           "title": "Success-Response:",
-          "content": "   HTTP/1.1 200 OK\n   {\n     \"id\": \"23\",\n     \"sensor\": \"Kitchen's temperature\"\n     \"room_id\": \"45\"\n     \"unit\": \"Temperature.Celsius\"\n     \"data\": [{\n         \"value\": \"23\"\n         \"timestamp\": \"1391631784\"\n         }, ...\n         ]\n   }\n"
+          "content": "  HTTP/1.1 200 OK\n  {\n      \"identifier\": \"MOISTURE_3\",\n      \"model\": {\n          \"name\": \"moisture\",\n          \"_id\": \"5329b7fcb5cc66dd9d79c2f9\",\n          \"__v\": 0\n      },\n      \"hub\": \"5329b7fcb5cc66dd9d79c2f2\",\n      \"created_on\": \"2014-02-05T22:07:23.073Z\",\n      \"room\": \"5329b7fcb5cc66dd9d79c2f4\",\n      \"_id\": \"5329b7fcb5cc66dd9d79c308\",\n      \"__v\": 0,\n      \"data\": []\n  }\n"
         }
       ]
     },
@@ -294,90 +186,15 @@ define({ api: [
     "filename": "express/routes/sensor.coffee"
   },
   {
-    "type": "get",
-    "url": "/user/:id",
-    "title": "Read data of a user*",
-    "name": "GetUser",
-    "group": "User",
-    "parameter": {
-      "fields": {
-        "Parameter": [
-          {
-            "group": "Parameter",
-            "type": "String",
-            "field": "id",
-            "optional": false,
-            "description": "<code>id</code> of the User."
-          }
-        ]
-      }
-    },
-    "success": {
-      "fields": {
-        "Success 200": [
-          {
-            "group": "Success 200",
-            "type": "String",
-            "field": "firstname",
-            "optional": false,
-            "description": "First Name of the User."
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "field": "lastname",
-            "optional": false,
-            "description": "Last Name of the User."
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "field": "id",
-            "optional": false,
-            "description": "<code>id</code> of the User."
-          }
-        ]
-      }
-    },
-    "error": {
-      "fields": {
-        "401 Unauthorized": [
-          {
-            "group": "401",
-            "field": "NoApikey",
-            "optional": false,
-            "description": "No apikey was sent with the request."
-          },
-          {
-            "group": "401",
-            "field": "NoAccessRight",
-            "optional": false,
-            "description": "Wrong apikey."
-          }
-        ],
-        "404 Not Found": [
-          {
-            "group": "404",
-            "field": "IdNotFound",
-            "optional": false,
-            "description": "The <code>id</code> of the User was not found."
-          }
-        ]
-      }
-    },
-    "version": "0.0.0",
-    "filename": "express/routes/login.coffee"
-  },
-  {
     "type": "post",
-    "url": "/user/login",
+    "url": "/login/",
     "title": "Login to HomeAgain",
     "name": "PostUser",
     "group": "User",
     "examples": [
       {
         "title": "Example usage:",
-        "content": " curl -i \\\n     -H \"Content-Type: application/json\" \\\n     -d '{\"username\":\"user\", \"password\":\"password\"}' \\\n     \"http://homeagain.io/login\"\n"
+        "content": "curl -i \\\n    -H \"Content-Type: application/json\" \\\n    -d '{\"username\":\"user\", \"password\":\"password\"}' \\\n    \"http://homeagain.io/login\"\n"
       }
     ],
     "parameter": {
@@ -386,9 +203,9 @@ define({ api: [
           {
             "group": "Parameter",
             "type": "String",
-            "field": "id",
+            "field": "username",
             "optional": false,
-            "description": "<code>id</code> of the User."
+            "description": "<code>username</code> of the User."
           },
           {
             "group": "Parameter",
@@ -406,37 +223,23 @@ define({ api: [
           {
             "group": "Success 200",
             "type": "String",
-            "field": "firstname",
+            "field": "user_id",
             "optional": false,
-            "description": "First Name of the User."
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "field": "lastname",
-            "optional": false,
-            "description": "Last Name of the User."
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "field": "id",
-            "optional": false,
-            "description": "<code>id</code> of the User."
+            "description": "<code>user_id</code> of the User (ObjectId)"
           },
           {
             "group": "Success 200",
             "type": "String",
             "field": "api_key",
             "optional": false,
-            "description": "APIkey that needs to be sent with each request with a star."
+            "description": "APIkey that needs to be sent along with each request with a star."
           }
         ]
       },
       "examples": [
         {
           "title": "Success-Response:",
-          "content": "   HTTP/1.1 200 OK\n   {\n     \"firstname\": \"Matthieu\",\n     \"lastname\": \"Dupont\"\n     \"id\": \"23\"\n     \"api_key\": \"785876\"\n   }\n"
+          "content": "  HTTP/1.1 200 OK\n  {\n    \"id\": \"23\"\n    \"api_key\": \"785876\"\n  }\n"
         }
       ]
     },
